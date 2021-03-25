fe17eaa9910d1b340281bc8043fa870d4ee43331
hazel <liam.k.becker@gmail.com>
2021-03-25 13:54:57 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,36 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer moveCount1 = 0;
+        Integer moveCount2 = 0;
+        Integer currentIndex = startIndex;
+
+        while(true){
+            currentIndex++;
+            if(currentIndex == this.playList.length){
+                currentIndex = 0;
+            }
+            moveCount1++;
+            if(this.playList[currentIndex].equals(selection)){
+                break;
+            }
+        }
+
+        currentIndex = startIndex;
+        while(true){
+            currentIndex--;
+            if(currentIndex == -1){
+                currentIndex = this.playList.length - 1;
+            }
+            moveCount2++;
+            if(this.playList[currentIndex].equals(selection)){
+                break;
+            }
+        }
+
+        if(moveCount1 < moveCount2){
+            return moveCount1;
+        }
+        return moveCount2;
     }
 }
